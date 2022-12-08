79414af552cdd0adda55f82217979106c014499c
Gabogogi <bencitymaina@gmail.com>
2022-12-08 13:32:33 -0800

    g

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
+#include <stdlibh>
 /**
 * main -Entry point
 *
 * Description: 'the program'd description'
 *
-* Return: Always 0 (Success)
+* Return: Always 1 (Success)
 */
 int main(void)
 
 {
 
-	fprintf(stderr, "\"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1;
 
