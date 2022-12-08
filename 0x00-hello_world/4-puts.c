a14858f02647d05e0fb3555b8ef4ba6539c876e9
Gabogogi <bencitymaina@gmail.com>
2022-12-08 08:56:53 -0800

    g

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,25 +1,18 @@
 #include <stdio.h>
 
 /**
+ * main -Entry point
  *
- * * main -Entry point
+ * Description: 'the program'd description'
  *
- * *
- *
- * * Description: 'the program'd description'
- *
- * *
- *
- * * Return: Always 0 (Success)
- *
- * */
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 
 {
 
-	puts("Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle\n");
 
 	return (0);
-
 }
