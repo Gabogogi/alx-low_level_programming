8ab7104e098ea7214b255c7e73efc71377019708
Gabogogi <bencitymaina@gmail.com>
2022-12-08 11:09:08 -0800

    k

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Description: 'the program'd description'
+ *
+ * Return: Always 0 (Success) 
+ */
+
+int main(void)
+
+{
+
+	int i;
+
+	double d;
+
+	char c;
+
+
+
+	printf("The size of an int is: %lu.\n", (unsigned long) sizeof (i));
+
+	printf("The size of a double is: %lu.\n", (unsigned long) sizeof (d));
+
+	printf("The size of a char is: %lu.\n", (unsigned long) sizeof (c));
+
+
+
+	return (0);
+
+}
+
+
