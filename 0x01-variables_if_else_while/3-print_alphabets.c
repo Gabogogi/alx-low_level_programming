6158df3c8ccd3f4af2f603c603a226b0089f5f43
Gabogogi <bencitymaina@gmail.com>
2022-12-09 10:34:36 -0800

    g

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,14 +4,15 @@
  * Description: 'the program's description'
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	int ch;
+
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		putchar (ch);
 	}
-	for (ch = 'A'; ch <= 'Z'; ch ++)
+	for (ch = 'A'; ch <= 'Z'; ch++)
 	{
 		putchar (ch);
 	}
