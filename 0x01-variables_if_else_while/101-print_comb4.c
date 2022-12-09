41311353b902aee111173b54a20bc64aba1ae585
Gabogogi <bencitymaina@gmail.com>
2022-12-09 15:38:29 -0800

    h

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,11 +7,11 @@
 int main(void)
 {
 	int digit1, digit2, digit3;
-
+	
 	for (digit1 = 0; digit1 < 8; digit1++)
 	{
 		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
-		{						
+		{
 			for (digit3 = digit2 + 1; digit3 < 10; digit3++)
 			{						
 				putchar((digit1 % 10) + '0');
@@ -19,7 +19,7 @@ int main(void)
 				putchar((digit3 % 10) + '0');
 				
 				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
-					continue;
+				i	continue;
 				putchar(',');
 				putchar(' ');
 			}		
