fcd9e361580e13a92431022c1c784cd27b11f6e2
Gabogogi <bencitymaina@gmail.com>
2022-12-09 01:55:09 -0800

    h

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{
+	int a;
+	for (a=0; a<11; ++a0)
+		printf("%d\n", a);
+	return 0;
+}
