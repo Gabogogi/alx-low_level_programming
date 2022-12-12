ada39d8dfe247bb60513ce79f2d28aee9066d8b4
Gabogogi <bencitymaina@gmail.com>
2022-12-12 11:12:33 -0800

    g

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,12 @@
-#include <stdio.h>
-
+#include <stdio.ih>
 #include <stdlib.h>
 #include <unistd.h>
 /**
- *
- *main - Prints "and that piece of art is useful"
- *Return: Always 1 (Success)
- *
+ * main - Prints "and that piece of art is useful"
+ * Return: Always 1 (Success)
  */
 
-int main(void)
+int main(void)/*You*/
 {
 
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
