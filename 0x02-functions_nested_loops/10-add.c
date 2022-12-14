d02a6cf3462e3492feff146c029f60c4f624c7aa
Gabogogi <bencitymaina@gmail.com>
2022-12-14 13:39:55 -0800

    g

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - a function that adds two integers and returns the sum
+ * @one: first number
+ * @two: second number
+ * Return: one + two
+ */
+int add(int one, int two)
+{
+	return (one + two);
+}
