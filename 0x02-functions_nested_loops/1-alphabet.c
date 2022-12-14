90b44ebe7cc1fa8d88855a753e06220416ea2df1
Gabogogi <bencitymaina@gmail.com>
2022-12-13 22:51:22 -0800

    g

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (success)
+ */
+int main()
+	char c;
+	for(c = 'a'; c <= 'z'; c++){
+		putchar(c);
+	}
+	putchar('\n');
+return (0);
+}
