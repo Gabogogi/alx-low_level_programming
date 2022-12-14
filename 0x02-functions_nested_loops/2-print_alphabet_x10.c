977e80feac0e5b5de874ea9f30779d6dc31f1602
Gabogogi <bencitymaina@gmail.com>
2022-12-14 10:25:38 -0800

    g

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,17 @@
-#include <main.h>
+#include "main.h"
 /**
- *print_alphabet_x10 - print the laphabet 10 times in lowercase
+ *
+ * print_alphabet_x10 -prints the alphabet 10 times
  *
  */
-void print_alphabet_x10(void)
+void print_alphabet_x10(void);
 {
 	int count = 0;
 	char letter;
-	while(count++ <= 9)
+	while (count ++ <= 10)
 	{
-		for(letter = 'a'; letter <= 'z'; letter++)
-			putchar (letter);
+		for (letter = 'a'; letter <= 'z'; letter++)
+			putchar(letter);
 		putchar('\n');
 	}
+}
