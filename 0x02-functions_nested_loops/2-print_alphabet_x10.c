661e94c78230cb3f38ee0070c3ea8f556a500b91
Gabogogi <bencitymaina@gmail.com>
2022-12-14 10:27:14 -0800

    f

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,10 @@
-#include "main.h"
+#include <main.h>
 /**
  *
  * print_alphabet_x10 -prints the alphabet 10 times
  *
  */
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 	int count = 0;
 	char letter;
