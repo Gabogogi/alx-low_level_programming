d8a050a4e1db2093606c36876c9e8c9773e022db
Gabogogi <bencitymaina@gmail.com>
2022-12-15 09:57:50 -0800

    f

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - multiply two integers
+ * @a: integers to multiply
+ * @b: integer to multiply
+ * Return: addition of multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
