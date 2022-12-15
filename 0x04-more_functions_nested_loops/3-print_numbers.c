73107e5428974253a059229bc897aa787ace61ee
Gabogogi <bencitymaina@gmail.com>
2022-12-15 10:08:34 -0800

    g

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_numbers - prints numbers 0 to 9
+ * print_numbers - print 0-9
  * Return: void
  */
 void print_numbers(void)
@@ -10,6 +10,7 @@ void print_numbers(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
-		_putchar(c)
+		_putchar(c);
 	}
-	_putchar ('\n');
+	_putchar('\n');
+}
