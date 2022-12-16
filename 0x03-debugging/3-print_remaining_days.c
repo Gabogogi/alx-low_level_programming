2cc0f21c143b8de42ddf1921465a615112e271cb
Gabogogi <bencitymaina@gmail.com>
2022-12-15 16:53:39 -0800

    g

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -10,7 +10,7 @@
  */
 void print_remaining_days(int month, int day, int year)
 {
-	if (year % 4 == 0 || ((year % 100 == 0) && (year % 400 == 0))
+	if (year % 4 == 0 || ((year % 100 == 0) && (year % 400 == 0)))
 	{
 			if (month >= 3 && day >= 60)
 			{
@@ -28,7 +28,7 @@ void print_remaining_days(int month, int day, int year)
 			else
 			{
 				printf("Day of the year: %d\n", day);
-				printf("Remaining days: %d\n" - day);
+				printf("Remaining days: %d\n", 365 - day);
 			}
 	}
 }
