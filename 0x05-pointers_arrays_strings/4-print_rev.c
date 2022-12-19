1f4081f69354db57e5c8ce1be930fa069892f5bf
Gabogogi <bencitymaina@gmail.com>
2022-12-19 21:51:21 +0300

    g

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,9 +11,9 @@ void print_rev(char *s)
 
 	j = 0;
 
-        while (s[j] != '\0')
-        j++;
-        for (i = j - 1; i >= 0; i--)
+	while(s[j] != '\0')
+        	j++;
+        for(i = j - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
