32c223c7d3704d9bb6968c923f7afb628d5fa5cb
Gabogogi <bencitymaina@gmail.com>
2022-12-19 21:01:00 +0300

    g

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,9 +1,14 @@
 #include "main.h"
 /**
 * _strlen - returns length of a string
+* @s: string to be counted
 * Return: void
 */
 int _strlen(char *s)
 {
-	_putchar(strlen(s))	
+	int i; /* start the counter from 0*/
+
+	for (; s++;)
+		i++;
+	return (i);
 }
