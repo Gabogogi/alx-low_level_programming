13145ff190bbc3807d6f2a4fac74aef6badd8f6d
Gabogogi <bencitymaina@gmail.com>
2022-12-21 18:12:11 +0300

    g

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,11 +7,11 @@
 */
 void reverse_array(int *a, int n)
 {
-	int temp, i, j;
-	for(i = 0; j = n - 1; i < j; i++; j--)
+	int temp, i;
+	for(i = 0; i < n/2; i++)
 	{
 		temp = a[i];
-		a[i] = a[j];
-		a[j] = temp;
+		a[i] = a[n -1 - i];
+		a[n - 1 -i] = temp;
 	}
 }
