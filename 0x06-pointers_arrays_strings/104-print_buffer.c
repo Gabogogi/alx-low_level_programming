c85d93e1b74ca893b117335d56495eab222a7350
Gabogogi <bencitymaina@gmail.com>
2022-12-22 21:49:22 +0300

    g

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,15 +1,13 @@
-#include "main.h"                                             
-#include <stdio.h>                                            
-                                                              
-/**                                                           
- * print_buffer - prints a buffer                             
- * @b: buffer                                                 
- * @size: buffer size                                         
- * Return: No return                                          
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_buffer - prints a buffer
+ * @b: buffer
+ * @size: buffer size
+ * Return: No return
  */                                                           
 void print_buffer(char *b, int size)
-{
-	int j, k, l;
+{	int j, k, l;
 
 	if (size <= 0)
 		printf("\n");
