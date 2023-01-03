95dc9f1af83e5fcc1565a253d22129f6bd4d579b
Gabogogi <bencitymaina@gmail.com>
2023-01-03 16:24:07 +0300

    g

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,10 +10,8 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
-	char *src_char = (char *)src;
-	char *dest_char = (char *)dest;
-
+	
 	for (i = 0; i < n; i++)
-		dest_char[i] = src_char[i];
-	return (0);
+		dest [i] = src[i];
+	return (dest);
 }
