ba063b83961d5021f98beefbee2dc88837a9d497
Gabogogi <bencitymaina@gmail.com>
2023-01-03 05:10:00 -0800

    g

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -13,7 +13,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	char *csrc = (char *)src;
 	char *cdest = (char *)dest;
 
-	for (i = 0; i < n; i ++)
+	for (i = 0; i < n; i++)
 		cdest[i] = csrc[i];
 	return (0);
 }
