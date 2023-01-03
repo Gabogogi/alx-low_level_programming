a72c812a8b2f0fc5602fbbbbdcd06fd3261815c4
Gabogogi <bencitymaina@gmail.com>
2023-01-03 20:54:35 +0300

    g

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -7,17 +7,17 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int f, b, c, d = 0, e = 0;
+	int i, j, p, l = 0, r = 0;
 
-	for (f = 0; f < size; f++)
+	for (i = 0; i < size; i++)
 	{	
-	c = (f * size) + f;
-	d += *(a + c);
+		p = (i * size) + i;
+		l += *(a + p);
 	}
-	for (b = 0; b < size; b++)
+	for (j = 0; j < size; j++)
 	{
-		c = (b * size) + (size - 1 - b);
-		e += *(a + c);
+		p = (j * size) + (size - 1 - j);
+		r += *(a + p);
 	}
-	printf("%f, %f\n", d, e);
+	printf("%i, %i\n", l, r);
 }
