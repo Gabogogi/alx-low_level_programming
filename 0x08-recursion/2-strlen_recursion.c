82e446a60161da734891bf0cf8a0e9effc8f4b35
Gabogogi <bencitymaina@gmail.com>
2023-01-05 18:31:02 +0300

    f

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * strlen_recursion - returns the length of a string
+ * _strlen_recursion - returns the length of a string
  * @s: string to count
  * Return: Length of string s
  */
 int _strlen_recursion(char *s)
 {
-	if(*s == '\0')
+	if (*s == '\0')
 		return (1 + _strlen_recursion(s + 1));
 	return (0);
 }
