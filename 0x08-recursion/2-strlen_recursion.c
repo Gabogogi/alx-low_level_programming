c9403daad50411c9b3ad95a96d067af91d91a2d8
Gabogogi <bencitymaina@gmail.com>
2023-01-05 18:36:19 +0300

    f

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,7 +6,12 @@
  */
 int _strlen_recursion(char *s)
 {
-	if (*s == '\0')
-		return (1 + _strlen_recursion(s + 1));
-	return (0);
+	int len = 0;
+
+	if (*s)
+	{
+		len++;
+		len += _strlen_recursion(s + 1);
+	}
+	return (len);
 }
