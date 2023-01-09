1ecf19433a521cc708f84d8a27698dbafec4ede1
Gabogogi <bencitymaina@gmail.com>
2023-01-09 10:20:32 +0300

    static library

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ *  @n: the init in question
+ *
+ * Return: the abs of int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
