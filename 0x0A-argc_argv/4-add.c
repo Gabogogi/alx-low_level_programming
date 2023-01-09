9bcd19a9c348e53185daff0b4337075ac88708c8
Gabogogi <bencitymaina@gmail.com>
2023-01-09 12:32:04 +0300

    g

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - adds positive numbers
+ * @argc: argument count
+ * @argv: arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int c, d;
+	int sum = 0;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (c = 1; i < argc; c++)
+	{
+		for (d = 0; argv[c][d] != '\0'; d++)
+		{
+			if (argv[c][d] < '0' || argv[c][d] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum = sum + atoi(argv[c]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
