46cf8069809bba9ec6c7c1f2c148f4f60f17af87
Gabogogi <bencitymaina@gmail.com>
2023-01-20 14:34:16 +0300

    g

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -14,7 +14,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	va_start(ap, n);
 	if (separator == NULL)
-		separtor = "";
+		separator = "";
 
 	for (i = 0; i < n; i++)
 	{
